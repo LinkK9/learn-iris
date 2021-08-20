53ce32ace722aed245a90a5cc1d464867da1d1d3
Linh Trinh Viet <73121552+LinkK9@users.noreply.github.com>
2021-08-20 15:41:27 +0700

    Basic

--- basic/main.go
+++ basic/main.go
@@ -4,34 +4,18 @@ import "github.com/kataras/iris/v12"
 
 func main() {
     app := iris.New()
-    // Load all templates from the "./views" folder
-    // where extension is ".html" and parse them
-    // using the standard `html/template` package.
+    // load các template từ folder ".views"
+    // Sẽ parse các file có đuôi html
+    // sử dụng template HTML
     app.RegisterView(iris.HTML("./views", ".html"))
 
     // Method:    GET
-    // Resource:  http://localhost:8080
     app.Get("/", func(ctx iris.Context) {
-        // Bind: {{.message}} with "Hello world!"
+        // Gắn data biến {{.message}} bằng "Hello world!"
         ctx.ViewData("message", "Hello world!")
-        // Render template file: ./views/hello.html
+        // Render file: ./views/hello.html
         ctx.View("hello.html")
     })
-
-    // Method:    GET
-    // Resource:  http://localhost:8080/user/42
-    //
-    // Need to use a custom regexp instead?
-    // Easy;
-    // Just mark the parameter's type to 'string'
-    // which accepts anything and make use of
-    // its `regexp` macro function, i.e:
-    // app.Get("/user/{id:string regexp(^[0-9]+$)}")
-    app.Get("/user/{id:uint64}", func(ctx iris.Context) {
-        userID, _ := ctx.Params().GetUint64("id")
-        ctx.Writef("User ID: %d", userID)
-    })
-
-    // Start the server using a network address.
+    // Khởi động server ở cổng 8080
     app.Listen(":8080")
 }
